0c9107c60df5287ded197368f4e896b2971c7bc7
Bobby Wilson <bobbywilson0@gmail.com>
2015-05-24 20:21:26 -0600

    setup application complete

--- src/gameboard/core.cljs
+++ src/gameboard/core.cljs
@@ -3,16 +3,3 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
-
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
-
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
-
