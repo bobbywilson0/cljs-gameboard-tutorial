f93758fe31f339e666a69f4e4c4854e5da946f62
Bobby Wilson <bobbywilson0@gmail.com>
2015-11-21 14:30:56 -0700

    add devcards

--- project.clj
+++ project.clj
@@ -5,60 +5,42 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [devcards "0.2.1"]
+                 [sablono "0.4.0"]
+                 #_[org.omcljs/om "0.9.0"]
+                 #_[reagent "0.5.1"]]
 
-  :plugins [[lein-cljsbuild "1.1.0"]
-            [lein-figwheel "0.4.0"]]
+  :plugins [[lein-cljsbuild "1.1.1"]
+            [lein-figwheel "0.5.0-1"]]
 
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "target"]
+  
   :source-paths ["src"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-  
   :cljsbuild {
-    :builds [{:id "dev"
-              :source-paths ["src"]
+              :builds [{:id "devcards"
+                        :source-paths ["src"]
+                        :figwheel { :devcards true } ;; <- note this
+                        :compiler { :main       "gameboard.core"
+                                    :asset-path "js/compiled/devcards_out"
+                                    :output-to  "resources/public/js/compiled/gameboard_devcards.js"
+                                    :output-dir "resources/public/js/compiled/devcards_out"
+                                    :source-map-timestamp true }}
+                       {:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main       "gameboard.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/gameboard.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:main       "gameboard.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/gameboard.js"
+                                   :optimizations :advanced}}]}
 
-              :figwheel { :on-jsload "gameboard.core/on-js-reload" }
-
-              :compiler {:main gameboard.core
-                         :asset-path "js/compiled/out"
-                         :output-to "resources/public/js/compiled/gameboard.js"
-                         :output-dir "resources/public/js/compiled/out"
-                         :source-map-timestamp true }}
-             {:id "min"
-              :source-paths ["src"]
-              :compiler {:output-to "resources/public/js/compiled/gameboard.js"
-                         :main gameboard.core                         
-                         :optimizations :advanced
-                         :pretty-print false}}]}
-
-  :figwheel {
-             ;; :http-server-root "public" ;; default and assumes "resources" 
-             ;; :server-port 3449 ;; default
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Start an nREPL server into the running figwheel process
-             ;; :nrepl-port 7888
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is for simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
-             })
+  :figwheel { :css-dirs ["resources/public/css"] })
--- src/gameboard/core.cljs
+++ src/gameboard/core.cljs
@@ -1,101 +1,32 @@
-(ns ^:figwheel-always gameboard.core
-  (:require [goog.dom :as dom]
-            [goog.graphics :as graphics]
-            [goog.events :as events]))
+(ns gameboard.core
+  (:require
+   #_[om.core :as om :include-macros true]
+   [sablono.core :as sab :include-macros true]
+   [goog.graphics :as graphics])
+  (:require-macros
+   [devcards.core :as dc :refer [defcard deftest]]))
 
 (enable-console-print!)
 
-(def tile-size 50)
-(def tile-offset (/ tile-size 2))
-
-(def piece-radius 20)
-
-(def black (graphics/SolidFill. "black"))
-(def red (graphics/SolidFill. "red"))
-(def blue (graphics/SolidFill. "blue"))
-
-
-(def black-stroke (graphics/Stroke. 0.5 "black"))
-(def white-stroke (graphics/Stroke. 3 "white"))
-
 (def board-width 8)
 (def board-height 8)
-
-(def game-state
-  (atom
-    {:units
-     [{:team :black, :x 1, :y 0},
-      {:team :black, :x 3, :y 0},
-      {:team :black, :x 5, :y 0},
-      {:team :black, :x 7, :y 0},
-      {:team :black, :x 0, :y 1},
-      {:team :black, :x 2, :y 1},
-      {:team :black, :x 4, :y 1},
-      {:team :black, :x 6, :y 1},
-      {:team :black, :x 1, :y 2},
-      {:team :black, :x 3, :y 2},
-      {:team :black, :x 5, :y 2},
-      {:team :black, :x 7, :y 2},
-      {:team :red, :x 0, :y 5},
-      {:team :red, :x 2, :y 5},
-      {:team :red, :x 4, :y 5},
-      {:team :red, :x 6, :y 5},
-      {:team :red, :x 1, :y 6},
-      {:team :red, :x 3, :y 6},
-      {:team :red, :x 5, :y 6},
-      {:team :red, :x 7, :y 6},
-      {:team :red, :x 0, :y 7},
-      {:team :red, :x 2, :y 7},
-      {:team :red, :x 4, :y 7},
-      {:team :red, :x 6, :y 7}]}))
+(def tile-size 50)
 
 (def board (graphics/CanvasGraphics. (* board-width tile-size) (* board-height tile-size)))
-(def board-dom (.createDom board))
+(.createDom board)
 
+(defcard first-card
+  (sab/html [:div
+             [:h1 "This is your first devcard!"]]))
 
-(defn tile-color [x y]
-  (if (= (even? x) (even? y))
-    red
-    black))
+(defn main []
+  ;; conditionally start the app based on wether the #main-app-area
+  ;; node is on the page
+  (if-let [node (.getElementById js/document "main-app-area")]
+    (js/React.render (sab/html [:div "This is working"]) node)))
 
-(defn unit-color [unit]
-  (if (= :red (:team unit))
-    red
-    black))
+(main)
 
-(defn draw-tile! [x y color]
-  (.drawRect board x y tile-size tile-size black-stroke color))
+;; remember to run lein figwheel and then browse to
+;; http://localhost:3449/cards.html
 
-(defn board-to-client [x y]
-  (map #(+ (* tile-size %) tile-offset) [x y]))
-
-(defn client-to-board [x y]
-  (map #(- (.ceil js/Math (/ % tile-size)) 1) [x y]))
-
-(defn draw-unit! [unit]
-  (let [[x y] (board-to-client (:x unit) (:y unit))]
-    (.drawEllipse board x y piece-radius piece-radius white-stroke (unit-color unit))))
-
-(defn draw-units! []
-  (mapv draw-unit! (:units @game-state)))
-
-(defn draw-board! []
-  (mapv
-    (fn [y]
-      (mapv
-        (fn [x] (draw-tile! (* tile-size x) (* tile-size y) (tile-color x y)))
-        (range 0 board-width)))
-    (range 0 board-height)))
-
-(draw-board!)
-(draw-units!)
-
-(.render board)
-(events/listen
-  (aget (.getElementsByTagName js/document "canvas") 0)
-  events/EventType.MOUSEDOWN
-  (fn [e]
-    ;(.render board)
-    (.drawRect board (.-offsetX e) (.-offsetY e) 10 10 white-stroke blue)))
-
-;(println (client-to-board (.-offsetX e) (.-offsetY e)))
